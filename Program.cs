bdbcad483c63152d88612acccddfc3c206572918
DreamsitecoAdmin <elehgilbert@gmail.com>
2025-11-24 04:15:42 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+var bytes = new byte[64];
+using (var rng = RandomNumberGenerator.Create())
+{
+    rng.GetBytes(bytes);
+}
+var key = Convert.ToBase64String(bytes);
+Console.WriteLine(key);
+
+
+
